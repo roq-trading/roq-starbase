cc263196cf8c69ac2a94c605ae911e47e0ce97ec
thraneh <thraneh@gmail.com>
2026-04-10 06:12:00 +0000

    update

--- src/roq/starbase/filter/test.sh
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/env bash
-
-CONFIG_FILE="$HOME/dev/roq-dev/roq-deribit/share/prod/channels.json" 
-
-./roq-deribit-filter \
-  --type tcpdump \
-  --multicast_config_file $CONFIG_FILE \
-  --multicast_channel_ids 1,2,3,4,7,10,13,21
