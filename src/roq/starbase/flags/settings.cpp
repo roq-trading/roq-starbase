d353ee1ca296d61eb7e3d93efe260f0b3973dc11
thraneh <thraneh@gmail.com>
2026-06-11 06:34:08 +0000

    send git-hash to license server

--- src/roq/starbase/flags/settings.cpp
+++ src/roq/starbase/flags/settings.cpp
@@ -11,9 +11,9 @@ namespace starbase {
 namespace flags {
 
 Settings::Settings(args::Parser const &args)
-    : server::flags::Settings{args, ROQ_PACKAGE_NAME, ROQ_BUILD_NUMBER}, flags::Flags{flags::Flags::create()}, misc{flags::Misc::create()},
-      fix{flags::FIX::create()}, ws{flags::WS::create()}, multicast{flags::Multicast::create()}, mbp{flags::MBP::create()}, request{flags::Request::create()},
-      rest{flags::REST::create()} {
+    : server::flags::Settings{args, ROQ_PACKAGE_NAME, ROQ_BUILD_NUMBER, ROQ_GIT_DESCRIBE_HASH, {}}, flags::Flags{flags::Flags::create()},
+      misc{flags::Misc::create()}, fix{flags::FIX::create()}, ws{flags::WS::create()}, multicast{flags::Multicast::create()}, mbp{flags::MBP::create()},
+      request{flags::Request::create()}, rest{flags::REST::create()} {
   log::info("settings={}"sv, *this);
 }
 
